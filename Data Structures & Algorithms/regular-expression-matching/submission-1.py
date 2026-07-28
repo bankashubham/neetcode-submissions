de693d1eea7c4fb08df21c377f324b0e8ea7b793
Shubham Banka <banka.shubham@gmail.com>
2026-07-28 14:03:48 +0530

    Add: regular-expression-matching - submission-1

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-1.py
@@ -0,0 +1,34 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        m, n = len(s), len(p)
+
+        dp = [[False] * (n + 1) for _ in range(m + 1)]
+
+        # Base case
+        dp[m][n] = True
+
+        # Initialize the last row (empty string vs pattern)
+        for j in range(n - 2, -1, -1):
+            if p[j + 1] == '*':
+                dp[m][j] = dp[m][j + 2]
+
+        # Fill the remaining table
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+
+                first_match = (s[i] == p[j] or p[j] == '.')
+
+                if j + 1 < n and p[j + 1] == '*':
+                    dp[i][j] = (
+                        dp[i][j + 2]
+                        or
+                        (first_match and dp[i + 1][j])
+                    )
+                else:
+                    dp[i][j] = (
+                        first_match
+                        and
+                        dp[i + 1][j + 1]
+                    )
+
+        return dp[0][0]
